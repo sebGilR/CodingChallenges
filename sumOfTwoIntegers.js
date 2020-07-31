668bc263d6544a1fc6da15f6d104f4c623c0a670
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-31 15:27:10 -0500

    Sum of two integers challenge

---
+++ sumOfTwoIntegers.js
@@ -0,0 +1,20 @@
+const convert = n => {
+  let binary = '';
+  let temp = n;
+  
+  while (temp > 0) {
+    if (temp % 2 === 0) {
+      binary = '0' + binary;
+    } else {
+      binary = '1' + binary;
+    }
+    
+    temp = Math.floor(temp / 2);
+  }
+  
+  return binary
+}
+
+var getSum = function(a, b) {
+  return convert(b) | convert(a)
+};
