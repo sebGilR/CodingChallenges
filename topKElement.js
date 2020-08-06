b697161009ba2bd205a2abf74adbb3290a242eed
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-08-05 22:02:27 -0500

    Start topk frequent, not completed

---
+++ topKElement.js
@@ -0,0 +1,14 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} k
+//  * @return {number[]}
+//  */ NOT COMPLETED
+var topKFrequent = function(nums, k) {
+  const visited = []
+  const topFrequent = []
+  
+  for (let i = 0; i < nums.length; i++) {
+  }
+  
+  return visited
+};
