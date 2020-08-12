366c252415ca8ec1d1cb618c7aae96fdc137df50
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-08-12 17:50:50 -0500

    Add selection sort algorithm

---
+++ sort/selection_sort.rb
@@ -0,0 +1,14 @@
+# O(n^2)
+def selection_sort(arr)
+  n = arr.length
+
+  for i in 0...n
+    for j in i + 1...n
+      arr[j], arr[i] = arr[i], arr[j] if  arr[i] > arr[j]
+    end
+  end
+
+  return arr
+end
+
+p selection_sort([4, 3, 5, 1, 0, 7, 10, 2])
